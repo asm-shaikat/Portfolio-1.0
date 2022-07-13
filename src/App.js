61b4ced19e8befdd8fbfc3fdfb7f616a102cb72c
Abu Shadat Shaikat <asmshaikat1952@gmail.com>
2022-07-13 22:37:17 +0600

    works with skill module

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './components/home/Home';
 import Skills from './components/skills/Skills';
 import Projects from './components/projects/Projects';
-import Frontend from './components/skills/frontend/Frontend';
-import Backend from './components/skills/backend/Backend';
-import Database from './components/skills/database/Database';
 function App() {
   return (
     <div>
@@ -14,9 +11,6 @@ function App() {
           <Route path='/' element={<Home/>} />
           <Route path='/skills' element={<Skills/>} ></Route>
           <Route path='/projects' element={<Projects/>}></Route>
-          <Route path='/frontend' element={<Frontend/>}></Route>
-          <Route path='/backend' element={<Backend/>}></Route>
-          <Route path='/database' element={<Database/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,21 +1,72 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Zoom from 'react-reveal/Zoom';
+import Fade from 'react-reveal/Fade';
+import './skills.css'
+import Navbar from '../navbar/Navbar';
 const Skills = () => {
     return (
         <>
-            {/* Skills */}
-            <div className="skills w-full mt-20 mb-10">
-                <p className='text-center font-sans text-3xl font-extrabold tracking-[.25em]'>Skills</p>
-                <div className="skill-section flex w-4/6 ml-14 mt-10 xl:ml-56">
-                        <div className='font-serif m-auto bg-orange-500 p-2'>
-                            <button><Link to="/frontend">FrontEnd</Link></button>
-                        </div>
-                        <div className='font-serif m-auto bg-orange-500 p-2'>
-                            <button><Link to="/backend">BackEnd</Link></button>
-                        </div>
-                        <div className='font-serif m-auto bg-orange-500 p-2'>
-                            <button><Link to="/database">Database</Link></button>
-                        </div>
+            <Navbar></Navbar>
+                <div className="text-center skill-title border-2 border-purple-800 w-4/5 rounded-md ml-10 mb-5 mt-5 lg:ml-28">
+                    <p>SKILLS</p>
+                </div>
+            <div className="skill-section ml-10 lg:flex lg:ml-96">
+                <div className="fend-bend flex mt-10 space-x-6">
+                    <div className="fend skill-container">
+                        <Fade left>
+                            <p className='text-black font-semibold'>Front End</p>
+                        </Fade>
+                        <Zoom left cascade>
+                            <ul>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>JavaScript</li>
+                                <li>React</li>
+                                <li>Bootstrap</li>
+                                <li>Tailwind CSS</li>
+                            </ul>
+                        </Zoom>
+                    </div>
+                    <div className="bend skill-container">
+                        <Fade left>
+                            <p className='text-black font-semibold'>Back End & Database</p>
+                        </Fade>
+                        <Zoom left cascade>
+                            <ul>
+                                <li>PHP</li>
+                                <li>Laravel</li>
+                                <li>MySQL</li>
+                                <li>API</li>
+                            </ul>
+                        </Zoom>
+                    </div>
+                </div>
+                <div className="othhers-and-os flex mt-10 space-x-6 lg:ml-4">
+                    <div className="others-skill skill-container">
+                        <Fade left>
+                            <p className='text-black font-semibold'>Others Skill</p>
+                        </Fade>
+                        <Zoom left cascade>
+                            <ul>
+                                <li>UI Design</li>
+                                <li>Adobe Illustator</li>
+                                <li>Digital Marketing</li>
+                            </ul>
+                        </Zoom>
+                    </div>
+
+                    <div className="Operating-system skill-container">
+                        <Fade left>
+                            <p className='text-black font-semibold'>Operating System</p>
+                        </Fade>
+                        <Zoom left cascade>
+                            <ul>
+                                <li>Ubuntu</li>
+                                <li>Windows</li>
+                            </ul>
+                        </Zoom>
+                    </div>
+
                 </div>
             </div>
         </>
