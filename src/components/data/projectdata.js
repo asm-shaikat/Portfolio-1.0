59b1ae4618091bf98b95e310f0578340f70f5a8b
Abu Shadat Shaikat <asmshaikat1952@gmail.com>
2022-10-06 17:52:20 +0600

    few projects added

--- src/components/data/projectdata.js
+++ src/components/data/projectdata.js
@@ -1,19 +1,78 @@
 const projectdata =[
     {
+        title: 'BAUST Mechatronics & Robotics Club',
+        technology: 'HTML | CSS | Tailwind | JavaScript | React | ',
+        image: 'project-img6.png',
+        altName: 'Robotics',
+        type: 'React',
+        link: 'https://github.com/asm-shaikat/bmrc-landing-page',
+        live: 'https://baustmrc.netlify.app/',
+        description: 'UI for BAUST Mechatronics & Robotics Club'
+
+    },
+    {
+        title: 'Gaget Era',
+        technology: 'HTML | CSS | JavaScript |',
+        image: 'project-img4.png',
+        altName: 'Gaget Era',
+        type: 'Rowproject',
+        link: 'https://github.com/asm-shaikat/rapid-clone-pages',
+        live: 'https://gagetera.netlify.app/',
+        description: 'Landing page for ecommerce website'
+
+    },
+    {
         title: 'Hospital Management System',
         technology: 'HTML | CSS | JavaScript | MySQL | PHP',
-        image: '/project-img/Hospital.png',
+        image: 'project-img2.png',
         altName: 'Hospital',
         type: 'Rowproject',
+        link: 'https://github.com/asm-shaikat/Hospital-Management-System',
+        live: '',
         description: 'Hospital Management System is a system enabling hospitals to manage information and data related to all aspects of healthcare – processes, providers, patients, and more, which in turn ensures that processes are completed swiftly and effectively.'
     },
+
+    {
+        title: 'Panda Store',
+        technology: 'HTML | CSS | JavaScript',
+        image: 'project-img4.png',
+        altName: 'Panda Store',
+        type: 'Rowproject',
+        link: 'https://github.com/asm-shaikat/rapid-clone-pages-1',
+        live: 'https://mypandastore.netlify.app/',
+        description: 'Landing page for ecommerce website'
+    },
+
     {
         title: 'E-Commerce Web Application',
         technology: 'HTML | CSS | Tailwind | JavaScript | MySQL | Laravel ',
-        image: '/project-img/Ecommerce.png',
+        image: 'project-img1.png',
         altName: 'Ecommerce',
         type: 'Laravel',
+        link: 'https://github.com/asm-shaikat/EcommerceApp',
+        live: '',
         description: 'A website that allows people to buy and sell physical goods, services, and digital products over the internet rather than at a brick-and-mortar location. Through an e-commerce website, a business can process orders, accept payments, manage shipping and logistics, and provide customer service.'
+    },
+    {
+        title: 'Student Management System',
+        technology: 'HTML | CSS | Tailwind | JavaScript | MySQL | Laravel ',
+        image: 'project-img7.png',
+        altName: 'EMS',
+        type: 'Laravel',
+        link: 'https://github.com/asm-shaikat/baust-ems-lite',
+        live: '',
+        description: 'Managing students and employees information and semester fees. Every users have their own profile.'
+    },
+    {
+        title: 'Social Media UI',
+        technology: 'HTML | CSS | Tailwind | JavaScript | React | ',
+        image: 'project-img3.png',
+        altName: 'Social Media',
+        type: 'React',
+        link: '',
+        live: 'https://baustsm.netlify.app/',
+        description: 'This social media is for only students and faculty member of Bangladesh Army University of Science and Technology (BAUST), Saidpur.It is on development stage.'
+
     },
 ]
 export default projectdata;
--- src/components/projects/Projectcontiainer.js
+++ src/components/projects/Projectcontiainer.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import './projects.css';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./projects.css";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+
 const Projectcontiainer = (req) => {
-    const {title,technology,image,altName,description,type} = req.data;
+    const { title, technology, image, altName, description,link,live } = req.data;
     return (
-        <div className='project-card grid place-items-center'>
-            <div className="child flex ">
-                <p className='lg:p-2 p-1 m-1 lg:m-2 border-b-2 lg:border-b-2 border-b-yellow-600 text-xs lg:text-xl'>{title}</p>
+        <div className='project-card grid place-items-center mb-4'>
+            <div className="child flex">
+                <p className='text-sm lg:p-2 p-1 m-1 lg:m-2 border-b-2 lg:border-b-2 border-b-yellow-600  lg:text-xl'>{title}</p>
                 <p className='p-1 m-1 border-b-2 border-b-yellow-600 text-sm lg:text-xl'>{technology}</p>
+                <p className='p-4 text-red-900 underline cursor-pointer'><a href={`${link}`} target="_">Show code</a></p>
             </div>
+            <p className='p-4 text-sm text-red-900'><a href={`${live}`} target="_">Watch Live</a></p>
+            <small className='hidden lg:block'>Put cursor on image to see details</small>
             <div className="child-1 w-3/5">
-                <img src={image} alt={altName} />
+                <img src={`project-img/${image}`} alt={altName} />
             </div>
-            <div className="child-2 w-3/5   p-20 lg:-mt-96 text-black  bg-white bg-opacity-50 text-center">
+            <div className="child-2 w-3/5 backdrop-blur-lg  p-20 lg:-mt-96 text-white  bg-blue-500 bg-opacity-50 text-center">
                 <p>{description}</p>
             </div>
-        </div>
+        </div> 
     );
 };
 
