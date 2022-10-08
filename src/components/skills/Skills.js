9aba9db3aba440512c0071b0a4917dc7d906b9bf
Abu Shadat Shaikat <asmshaikat1952@gmail.com>
2022-10-08 16:03:18 +0600

    update: skill secction updateed

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,76 +1,51 @@
 import React from 'react';
-import Zoom from 'react-reveal/Zoom';
-import Fade from 'react-reveal/Fade';
-import './skills.css'
+import Pulse from 'react-reveal/Pulse';
+import './skills.css';
+import '../navbar/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReact,faHtml5,faCss3,faJs,faPython,faPhp,faLaravel,faUbuntu,faLinux,faWindows } from '@fortawesome/free-brands-svg-icons' 
 import Navbar from '../navbar/Navbar';
+
 const Skills = () => {
     return (
-        <>
+        <div className='bg-gray-900'>
             <Navbar></Navbar>
                 <div className="text-center skill-title border-2 border-purple-800 w-4/5 rounded-md ml-10 mb-5 mt-5 lg:ml-28">
-                    <p>SKILLS</p>
+                    <p className='text-white'>SKILLS</p>
                 </div>
-            <div className="skill-section ml-8 lg:flex lg:ml-96">
-                <div className="fend-bend flex mt-10 space-x-6">
-                    <div className="fend skill-container">
-                        <Fade left>
-                            <p className='text-black font-semibold'>Front End</p>
-                        </Fade>
-                        <Zoom left cascade>
-                            <ul>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                                <li>React</li>
-                                <li>Bootstrap</li>
-                                <li>Tailwind CSS</li>
-                            </ul>
-                        </Zoom>
-                    </div>
-                    <div className="bend skill-container">
-                        <Fade left>
-                            <p className='text-black font-semibold'>Back End & Database</p>
-                        </Fade>
-                        <Zoom left cascade>
-                            <ul>
-                                <li>PHP</li>
-                                <li>Laravel</li>
-                                <li>MySQL</li>
-                                <li>API</li>
-                            </ul>
-                        </Zoom>
-                    </div>
-                </div>
-                <div className="othhers-and-os flex mt-10 space-x-6 lg:ml-4">
-                    <div className="others-skill skill-container">
-                        <Fade left>
-                            <p className='text-black font-semibold'>Others Skill</p>
-                        </Fade>
-                        <Zoom left cascade>
-                            <ul>
-                                <li>UI Design</li>
-                                <li>Adobe Illustator</li>
-                                <li>Digital Marketing</li>
-                            </ul>
-                        </Zoom>
-                    </div>
-
-                    <div className="Operating-system skill-container">
-                        <Fade left>
-                            <p className='text-black font-semibold'>Operating System</p>
-                        </Fade>
-                        <Zoom left cascade>
-                            <ul>
-                                <li>Ubuntu</li>
-                                <li>Windows</li>
-                                <li>Elementary OS</li>
-                            </ul>
-                        </Zoom>
-                    </div>
-
+           <div className='p-4'><p className='text-white ml-10  border-b-2 w-20 border-cyan-300 lg:text-2xl'>Frontend</p></div>
+             <div className='w-full'>
+                <div className='grid grid-cols-3 grid-row-3 lg:grid-row-2  lg:grid-cols-4  w-4/5 lg:w-3/5 ml-10 lg:ml-52'>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><FontAwesomeIcon className='text-yellow-500 fa-5x' icon={faJs} /><p className='text-white'>JavaScript</p></div>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><FontAwesomeIcon className='text-cyan-600  fa-5x' icon={faReact} /><p className='text-white'>React</p></div>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><img className='w-14 ml-4 pb-6  lg:pb-6 lg:ml-20' src="skill-img/tailwind.png" alt="" /><p className='text-white'>Tailwind</p></div>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><img className='lg:ml-16 ml-2 pb-4 w-20' src="skill-img/bootstrap.png" alt="" /><p className='text-white'>BootStrap</p></div>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><FontAwesomeIcon className='text-blue-600  fa-5x' icon={faCss3} /><p className='text-white'>CSS</p></div>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><FontAwesomeIcon className='text-green-600  fa-5x' icon={faHtml5} /><p className='text-white'>HTML</p></div>
                 </div>
             </div>
-        </>
+            
+
+            <div className='p-4'><p className='text-white ml-10  border-b-2 w-20 border-cyan-300 lg:text-2xl'>Backend</p></div>
+            <div className='w-full '>
+                <div className='grid grid-cols-3 grid-row-3 lg:grid-row-2  lg:grid-cols-4  w-4/5 lg:w-3/5 ml-10 lg:ml-52'>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><img className='ml-6 lg:ml-20 w-42 pt-2 mb-6' src="skill-img/cpp.svg" alt="" /><p className='text-white'>CPP</p></div>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><FontAwesomeIcon className='text-green-600 fa-5x' icon={faPython} /><p className='text-white'>Python</p></div>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><FontAwesomeIcon className='text-blue-600  fa-5x' icon={faPhp} /><p className='text-white'>PHP</p></div>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><FontAwesomeIcon className='text-orange-600  fa-5x' icon={faLaravel} /><p className='text-white'>Laravel</p></div>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><img className='lg:ml-10 w-38 pt-2 mb-6' src="skill-img/mysql.svg" alt="" /><p className='text-white'>MySQL</p></div>
+                </div>
+            </div>
+
+            <div className='p-4'><p className='text-white ml-10  border-b-2 w-10 border-cyan-300 lg:text-2xl'>OS</p></div>
+            <div className='w-full'>
+                <div className='grid grid-cols-3 grid-row-3 lg:grid-row-2  lg:grid-cols-4  w-4/5 lg:w-3/5 ml-10 pb-10 lg:ml-52'>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><FontAwesomeIcon className='text-orange-600 fa-5x' icon={faUbuntu} /><p className='text-white'>Ubuntu</p></div>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><FontAwesomeIcon className='text-white  fa-5x' icon={faLinux} /><p className='text-white'>elementary os</p></div>
+                    <div className='bg-slate-800 text-center p-2 border  border-yellow-700'><FontAwesomeIcon className='text-blue-400  fa-5x' icon={faWindows} /><p className='text-white'>Windows</p></div>
+                </div>
+            </div>
+           </div>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  'fontawesome-svg-core': {
+    'license': 'free'
+  },
+  plugins: ['macros'],
 }
